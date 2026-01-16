e968b87f4fd658ce3d4062c16aae56a8362f1add
CHUIZ OSORIO JUAN DAVID <202214036@uns.edu.pe>
2026-01-16 09:20:12 -0500

    depuracion de codigo post prueba integración

--- main.py
+++ main.py
@@ -3,23 +3,45 @@ import sys
 import os
 from fastapi import BackgroundTasks, FastAPI, HTTPException
 
+# Agregar src al path para importaciones
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src/'))
+
 #Dependencis Internas
 from src.retiro_atm.schema.input_retiro_atm import InputDataRetiroAtm
 from src.retiro_atm.schema.output_retiro_atm import OutputDataRetiroAtm
 from src.retiro_atm.service.service_prediction_retiro_atm import ServicioPredicticionRetiroAtm
-from fastapi import FastAPI
 
-# Agregar src al path para importaciones
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src/'))
+# 2. Importaciones
+try:
+    from fuga.schema.inputs import ChurnInput
+    from fuga.service.churn_service import churn_service
+except ImportError as e:
+    print(f"Error de importación: {e}")
+    sys.exit(1)
 
 # Iniciar la aplicación FastAPI
-app = FastAPI()
+app = FastAPI(
+    title="BankMind API",
+    description="API para detección de fraudes y otros modelos bancarios",
+    version="1.0.0"
+)
+
+# Importar Routers
+from src.fraude.router import router as fraud_router
+from src.morosidad.router import router as morosidad_router
+
+# Registrar Routers
+app.include_router(fraud_router)
+app.include_router(morosidad_router)
+
+#Instanciamos Servicios
+servicioPrediccionRetiro = ServicioPredicticionRetiroAtm()
 
 #Codigo base
-@app.post("/predecir/retiro",tags=["Predicción de Temperatura del Estátor"])
+@app.post("/retiro_atm/predecir",tags=["Predicción del Retiro de Efectivo en ATM"])
 async def predecir_temperatura(input_data: InputDataRetiroAtm ) -> OutputDataRetiroAtm:
     """
-    Endpoint para predecir la temperatura dado un conjunto de datos de entrada.
+    Endpoint para predecir el monto ha retirar en un solo dia en un ATM.
     """
     try:
         return servicioPrediccionRetiro.predecir_retiro(input_data)
@@ -30,9 +52,18 @@ async def predecir_temperatura(input_data: InputDataRetiroAtm ) -> OutputDataRet
 @app.get("/vivo",tags=["Verificación de la disponibilidad de la api"])
 async def health():
     """
-    Endpoint para verificar si esta funcionando lka API
+    Endpoint para verificar si esta funcionando la API
     """
     return {"mensaje": "ESTOY VIVO."}
+
+@app.post("/fuga/predecir")
+def predict_churn(data: ChurnInput):
+    input_data = data.model_dump()
+    result = churn_service.predict(input_data)
+    
+    if "error" in result:
+        raise HTTPException(status_code=500, detail=result["error"])
+    return result
 
 #Inicializacion del servidor local
 if __name__ == "__main__":
